5466bd0925c77b2ae10dce4ce201e08db1c513dc
Kris Hom <5077617+krishom@users.noreply.github.com>
2023-12-04 18:12:54 -0800

    Restore URL matching and navigation

--- safeway/safeway.js
+++ safeway/safeway.js
@@ -1,10 +1,15 @@
 (function () {
+  var url = "/foru/coupons-deals.html"
+  if (window.location.href.indexOf(url) < 0) {
+    window.location.replace("https://www.safeway.com" + url)
+    return
+  }
   function clipAll() {
     const interval = 200
     const buttons = document.querySelectorAll(".grid-coupon-btn")
     if (buttons.length > 0) {
       document.querySelector(".btn.load-more").dispatchEvent(new Event("click"))
-      let delay = 0;
+      let delay = 0
       buttons.forEach((btn) =>
         setTimeout(
           () => btn.dispatchEvent(new Event("click")),
