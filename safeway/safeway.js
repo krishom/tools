3844d3593f7c6f3c723dff091f3cae8bddc844ef
Kris Hom <5077617+krishom@users.noreply.github.com>
2024-11-25 15:33:26 -0800

    Add prettier and close visible modals

--- safeway/safeway.js
+++ safeway/safeway.js
@@ -1,23 +1,38 @@
-(function () {
-  const url = "/foru/coupons-deals.html"
+(() => {
+  const url = "/foru/coupons-deals.html";
   if (window.location.href.indexOf(url) < 0) {
-    window.location.replace("https://www.safeway.com" + url)
-    return
+    window.location.replace("https://www.safeway.com" + url);
+    return;
   }
-  function clipAll() {
-    const interval = 250
-    const buttons = document.querySelectorAll(".btn-primary")
+  const scrollAndClick = (el) => {
+    el.scrollIntoView();
+    el.dispatchEvent(new Event("click"));
+  };
+  const clickIfVisible = (query) => {
+    [...document.querySelectorAll(query)].forEach((el) => {
+      if (el.offsetHeight > 0) {
+        el.click();
+      }
+    });
+  };
+  const clipAll = () => {
+    const interval = 333;
+    const buttons = document.querySelectorAll(".btn-primary");
     if (buttons.length > 0) {
-      document.querySelector(".btn.load-more").dispatchEvent(new Event("click"))
-      let delay = 0
+      scrollAndClick(document.querySelector(".btn.load-more"));
+      let delay = 0;
       buttons.forEach((btn) =>
         setTimeout(
-          () => btn.dispatchEvent(new Event("click")),
+          () => {
+            scrollAndClick(btn);
+            clickIfVisible("button.close");
+            clickIfVisible("button.btn.btn-default.btn-modal");
+          },
           (delay += interval)
         )
-      )
-      setTimeout(clipAll, delay + interval)
+      );
+      setTimeout(clipAll, delay + interval);
     }
-  }
-  clipAll()
-})()
+  };
+  clipAll();
+})();
